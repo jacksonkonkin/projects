d2bc265195e57ecfa73306a66aaa0a06cf2c354c
jackson <jacksonkonkin@gmail.com>
2023-07-12 20:53:34 -0700

    Grid and btn logic

--- minesweeper/main.py
+++ minesweeper/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import settings
 import utils
+from cell import Cell
 
 root = Tk()
 # Override the settings of the root window
@@ -9,26 +10,36 @@ root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.title("Minesweeper")
 root.resizable(False, False)
 
-top_frame = Frame(root,
-                  bg="black",
-                  width=settings.WIDTH,
-                  height=utils.height_prct(25)
-                  )
+top_frame = Frame(
+    root,
+    bg="black",
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
+)
 top_frame.place(x=0, y=0)
 
-left_frame = Frame(root,
-                   bg="black",
-                   width=utils.width_prct(25),
-                   height=utils.height_prct(75)
-                   )
+left_frame = Frame(
+    root,
+    bg="black",
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
 left_frame.place(x=0, y=utils.height_prct(25))
 
-center_frame = Frame(root,
-                     bg="black",
-                     width=utils.width_prct(75),
-                     height=utils.height_prct(75)
-                     )
+center_frame = Frame(
+    root,
+    bg="black",
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
 center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        cell = Cell()
+        cell.create_btn_object(center_frame)
+        cell.cell_btn_object.grid(row=x, column=y)
+
 
 # Run the window
 root.mainloop()
