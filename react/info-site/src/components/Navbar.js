619d927e79ff678a082c329e18049c56ae92a37c
jackson <jacksonkonkin@gmail.com>
2023-07-18 14:36:35 -0700

    finished first project

--- react/info-site/src/components/Navbar.js
+++ react/info-site/src/components/Navbar.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Icon from '../assets/reactjs-icon.png'
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <img src={Icon} alt='logo' />
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
   )
 }
 
